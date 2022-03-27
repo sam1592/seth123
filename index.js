4ff53e9c1692c540311d46311fdb54018416e0ea
Satyam Sheth <samsheth92@gmail.com>
2022-03-27 19:31:46 -0400

    commit 3

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
+const packageJson = require('./package.json');
+const welcome = require('cli-welcome');
  
-// Clears the console.
-clearConsole();
+welcome({
+  title: `Get to know Seth`,
+  version: `${packageJson.version}`,
+  description: `${packageJson.description}`,
+  tagLine: `by Seth`,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+});
+
+console.log(`
+NAME: ${packageJson.name}
+VERSION: ${packageJson.version}
+DESC: ${packageJson.description}
+`)
+
 console.log(`
   Satyam Sheth
   Software Enginer @ CVS Health
