7004d596a9b492e1fc7d6e26066ba8be6d5f8879
Satyam Sheth <samsheth92@gmail.com>
2022-03-27 19:01:43 -0400

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+console.log(`
+  Satyam Sheth
+  Software Enginer @ CVS Health
+
+  LinkindIn -> https://linkedin.com/snsheth55
+  Github    -> https://github.com/samsheth92
+`);
