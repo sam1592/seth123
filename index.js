2fdfcb8050d1de19a11c37169e4a76a451dd2520
Satyam Sheth <samsheth92@gmail.com>
2022-03-27 20:37:54 -0400

    add chalk

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 const packageJson = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
+const log = console.log;
  
 welcome({
   title: `Get to know Seth`,
@@ -13,14 +15,14 @@ welcome({
   clear: true,
 });
 
-console.log(`
+log(`
 NAME: ${packageJson.name}
 VERSION: ${packageJson.version}
 DESC: ${packageJson.description}
 `)
 
-console.log(`
-  Satyam Sheth
+log(`
+  ${chalk.blue(`Satyam Sheth`)}
   Software Enginer @ CVS Health
 
   LinkindIn -> https://linkedin.com/snsheth55
